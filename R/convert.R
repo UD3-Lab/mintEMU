662139382970ed371136981ee241db32a9ef7f24
alwil <alexandra.wilczynska@gmail.com>
2023-02-13 15:53:34 +0100

    move python files to installation

--- R/convert.R
+++ R/convert.R
@@ -11,18 +11,18 @@ convert_pdf_text <-function(pdf_filenames){
   pdf_list<- as.list(pdf_filenames)
 
   # Create virtual environment
-  if ( !file.exists(here::here("R","pyvenv") )) {
+  if ( !file.exists(here::here("inst/python", "pyvenv") )) {
     reticulate::install_python(version = '3.9.7')
-    reticulate::virtualenv_create(envname = here::here("R","pyvenv"),
+    reticulate::virtualenv_create(envname = here::here("inst/python","pyvenv"),
                                   version = "3.9.7")
-    reticulate::py_install("PyMuPDF == 1.21.0", envname = here::here("R","pyvenv"))
+    reticulate::py_install("PyMuPDF == 1.21.0", envname = here::here("inst/python","pyvenv"))
   }
 
   # Activate the virtual environment for python
-  reticulate::use_virtualenv(here::here("R","pyvenv"))
+  reticulate::use_virtualenv(here::here("inst/python","pyvenv"))
 
   # Load python script with functions to extract texts
-  reticulate::source_python(here::here("R","extract-text-pdf-python.py"))
+  reticulate::source_python(here::here("inst/python","extract-text-pdf-python.py"))
 
   text_list <- convert_pdf(pdf_list) %>% unlist()
 
