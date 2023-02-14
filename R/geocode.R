aee1a2dfec765de8d80f0b3c349799af07dd93dc
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-02-14 21:25:59 +0100

    Create function to visualise thesis locations

--- R/geocode.R
+++ R/geocode.R
@@ -10,3 +10,25 @@
 geocode_thesis_locations <- function(data) {
   tidygeocoder::geocode(data, "location", method = 'osm', lat = latitude , long = longitude)
 }
+
+
+#' Visualise thesis locations
+#'
+#' Show thesis locations on the map of the world.
+#'
+#' @param data Data frame with thesis data.
+#'
+#' @return A plot with thesis locations
+#' @export
+visualize_thesis_locations <- function(data) {
+  world <- ggplot2::map_data("world")
+  ggplot2::ggplot() +
+    ggplot2::geom_map(
+      data = world, map = world,
+      aes(long, lat, map_id = region),
+      color = "black", fill = "lightgray", size = 0.1
+    ) +
+    ggplot2::geom_point(data = data, aes(longitude, latitude), color = "red") +
+    ggplot2::coord_fixed() +
+    ggplot2::theme_void()
+}
