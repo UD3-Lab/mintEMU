7f40ba84a0e7a9f9ad1d911126e9cc931abd0710
alwil <alexandra.wilczynska@gmail.com>
2023-03-24 15:37:30 +0100

    included the namespace

--- tests/testthat/test-clean.R
+++ tests/testthat/test-clean.R
@@ -354,6 +354,7 @@ test_that("urbanism_stopwords function cleans the additional stopwords before ad
       "hut",
       "playground",
       "city",
+      "cities",
       "urban",
       "urbanism",
       "hab",
