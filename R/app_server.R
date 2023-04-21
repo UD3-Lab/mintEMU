13d53ffbf09ad07da6018c9f2f4d5fec8359d945
alwil <alexandra.wilczynska@gmail.com>
2023-04-21 14:17:40 +0200

    added reactive dataset and update filters functions

--- R/app_server.R
+++ R/app_server.R
@@ -1,5 +1,45 @@
 #' @export
-app_server <- function(input, output) {
+app_server <- function(session,input, output) {
+
+  ## Get the data  - temporarily added data file in the project. this will mimic the packages final behaviour
+
+  # data_path <- here::here("analysis", "data", "derived_data", "emu_theses_with_text.csv")
+  # emu_theses <- readr::read_csv(data_path)
+  # usethis::use_data(emu_theses, overwrite = TRUE)
+  data("emu_theses")
+
+
+  # Update filters
+
+  shiny::updateSelectInput(session,"year", label = "Select year:",
+                    choices = c('ALL', emu_theses$graduation_year |>
+                                          as.character() |>
+                                          unique()|>
+                                          sort()
+                                ),
+                    selected='ALL')
+
+  # shiny::updateSelectInput(session, "exchange", label = "Select exchange semester:",
+  #                   choices = c('ALL',emu_theses$exchange_semester |>
+  #                                 as.character() |>
+  #                                 unique()|>
+  #                                 sort()  ),
+  #                   selected='ALL')
+
+  # Create reactive data object
+ emu_reactive <- shiny::reactive({
+   emu_theses |>
+     dplyr::filter(graduation_year  %in%
+                     if('ALL' %in% input$country)
+                       unique(emu_theses$graduation_year)
+                     else
+                       input$country
+                   )
+
+ })
+
+ # Render plots
+
   output$distPlot <- shiny::renderPlot({
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2]
@@ -15,4 +55,14 @@ app_server <- function(input, output) {
       main = 'Histogram of waiting times'
     )
   })
+
+
+  output$thesismap <- shiny::renderPlot({
+
+    x <- geocode_thesis_locations(emu_reactive())
+
+    visualize_thesis_locations(x)
+
+  })
+
   }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,9 +1,15 @@
 #' @export
 app_ui <- function() {
   # Define UI for application
-  bs4Dash::dashboardPage(header = bs4Dash::dashboardHeader(title = "mintEMU"),
-
-                         sidebar = bs4Dash::dashboardSidebar(
+  bs4Dash::dashboardPage(header = bs4Dash::dashboardHeader(
+    title = bs4Dash::dashboardBrand(
+      title = "mintEMU",
+      color = "primary",
+      href = "https://urbandesigntudelft.nl/research/rbanism/",
+      image = "TUDelft_logo_rgb.png"
+      )
+    ),
+    sidebar = bs4Dash::dashboardSidebar(
                            bs4Dash::sidebarMenu(
                              id = 'tab',
                              bs4Dash::menuItem("Intro",
@@ -48,7 +54,11 @@ app_ui <- function() {
                          body = bs4Dash::dashboardBody(
                            shinyjs::useShinyjs(),
                            bs4Dash::tabItems(
-                             bs4Dash::tabItem(tabName = "intro"),
+                             bs4Dash::tabItem(tabName = "intro",
+                                              bs4Dash::box(
+                                                shiny::plotOutput("thesismap")
+                                                )
+                                              ),
                              bs4Dash::tabItem(tabName = "data"),
                              bs4Dash::tabItem(tabName = "topic",
                                               shiny::fluidRow(
