71f1929a5158180d1719c8c0125bf67a98c30cbb
alwil <alexandra.wilczynska@gmail.com>
2023-04-21 12:19:43 +0200

    run app as a part of a package

---
+++ R/app_ui.R
@@ -0,0 +1,69 @@
+#' @export
+app_ui <- function() {
+  # Define UI for application
+  bs4Dash::dashboardPage(header = bs4Dash::dashboardHeader(title = "mintEMU"),
+
+                         sidebar = bs4Dash::dashboardSidebar(
+                           bs4Dash::sidebarMenu(
+                             id = 'tab',
+                             bs4Dash::menuItem("Intro",
+                                               tabName = "intro",
+                                               icon = shiny::icon('globe')
+                                               ),
+                             bs4Dash::menuItem("Data overview",
+                                               tabName = "data",
+                                               icon = shiny::icon('magnifying-glass-chart')
+                                               ),
+                             bs4Dash::menuItem("Topic models",
+                                               tabName = "topic",
+                                               icon = shiny::icon('diagram-project')
+                                               ),
+                             # Filters
+                             shiny::selectInput("year",
+                                                label = "Select graduation year:",
+                                                choices = c('ALL'),
+                                                multiple = F,
+                                                selected = 'ALL'
+                                                ),
+                             shiny::selectInput("exchange",
+                                                label = "Select exchange semester:",
+                                                choices = c('ALL'),
+                                                multiple = F,
+                                                selected = 'ALL'
+                                                ),
+                             # Tool tips
+                             shinyBS::bsTooltip("year",
+                                                "Filter results by graduation year.",
+                                                "right",
+                                                options = list(container = "body")
+                                                ),
+                             shinyBS::bsTooltip("exchange",
+                                                "Filter results by exchange semester",
+                                                "right",
+                                                options = list(container = "body")
+                                                )
+                             )
+                           ),
+
+                         body = bs4Dash::dashboardBody(
+                           shinyjs::useShinyjs(),
+                           bs4Dash::tabItems(
+                             bs4Dash::tabItem(tabName = "intro"),
+                             bs4Dash::tabItem(tabName = "data"),
+                             bs4Dash::tabItem(tabName = "topic",
+                                              shiny::fluidRow(
+                                                bs4Dash::box(
+                                                  shiny::sidebarPanel(
+                                                    shiny::sliderInput("bins",
+                                                                "Number of bins:",
+                                                                min = 1,
+                                                                max = 50,
+                                                                value = 30
+                                                                ))),
+                                                bs4Dash::box(shiny::plotOutput("distPlot"))
+                                                )
+                                              )
+                             )
+                           )
+  )
+  }
