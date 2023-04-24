9fc417e211651195ffd1988ff224cbb8301e98af
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-04-24 17:26:53 +0200

    Update package data and script

--- data-raw/emu_text.R
+++ data-raw/emu_text.R
@@ -1,9 +1,17 @@
-# Get dataset URL ---------------------------------------------------------
-url_emu_text <- ""
+# Download and import the dataset -----------------------------------------
+emu_text_url <- here::here("analysis", "data", "derived_data", "emu_theses_with_text.csv")
+emu_text_raw_df <- readr::read_csv(emu_text_url)
 
-# Download the dataset ----------------------------------------------------
-emu_text <- readr::read_csv(url_emu_text)
+# Clean and geocode the dataset -------------------------------------------
 
-readr::write_csv(emu_text, here::here("inst", "extdata", "emu_text.csv"))
+emu_text_df <- mintEMU::geocode_thesis_locations(emu_text_raw_df)
 
-usethis::use_data(emu_text, overwrite = TRUE)
+emu_text_df$text_clean <- emu_text_df$text |>
+  mintEMU::clean_basic() |>
+  stringr::str_remove_all(mintEMU::find_meta_stopwords(emu_theses)) |>
+  stringr::str_remove_all(mintEMU::urbanism_stopwords(
+    add_stopwords = c("emu", "european postgraduate master in urbanism"))) |>
+  stringr::str_remove_all(mintEMU::thesis_stopwords(
+    add_stopwords = c("advisor", "prof")))
+
+usethis::use_data(emu_text_df, overwrite = TRUE)
