f34bf1235f4031da14416ddd5361545464491921
alwil <alexandra.wilczynska@gmail.com>
2023-06-16 15:55:53 +0200

    changes to app

--- R/app_server.R
+++ R/app_server.R
@@ -1,11 +1,13 @@
 #' @export
 app_server <- function(session,input, output) {
 
-  ## Use the thematic package to adjust plot styles to app theme:
+  ## Use the thematic package to adjust plot styles to app theme ---------------
   thematic::thematic_shiny()
   bs4Dash::useAutoColor()
 
-  ## Get the data  - temporarily added data file in the project. this will mimic the packages final behaviour
+  ## Get the data ---------------------- ---------------------------------------
+
+  ## temporarily added data file in the project. this will mimic the packages final behaviour
 
   # data_path <- here::here("analysis", "data", "derived_data", "emu_theses_with_text.csv")
   # emu_theses <- readr::read_csv(data_path)
@@ -13,68 +15,49 @@ app_server <- function(session,input, output) {
   data("emu_theses")
 
 
-  # Update filters
-
+  # Update filters -------------------------------------------------------------
   shiny::updateSelectInput(session,"year", label = "Select year:",
-                    choices = c('ALL', emu_theses$graduation_year |>
+                           choices = c('ALL', emu_theses$graduation_year |>
                                           as.character() |>
                                           unique()|>
-                                          sort()
-                                ),
-                    selected='ALL')
-
-  # Tool tips
-  bs4Dash::addTooltip(id = "year",
-                      options = list(
-                        title = "Filter results by graduation year",
-                        placement = "right")
-                      )
-
-  bs4Dash::addTooltip(id = "exchange",
-                      options = list(
-                        title = "Filter results by exchange semester",
-                        placement = "right")
-                      )
+                                          sort() ),
+                           selected='ALL')
 
   # shiny::updateSelectInput(session, "exchange", label = "Select exchange semester:",
-  #                   choices = c('ALL',emu_theses$exchange_semester |>
+  #                          choices = c('ALL',emu_theses$exchange_semester |>
   #                                 as.character() |>
   #                                 unique()|>
   #                                 sort()  ),
-  #                   selected='ALL')
+  #                          selected='ALL')
 
-  # Create reactive data object
- emu_reactive <- shiny::reactive({
+  # Tool tips ------------------------------------------------------------------
+  bs4Dash::addTooltip(id = "year",
+                      options = list(title = "Filter results by graduation year",
+                                     placement = "right"))
+
+  bs4Dash::addTooltip(id = "exchange",
+                      options = list(title = "Filter results by exchange semester",
+                                     placement = "right"))
+
+
+  # Create reactive data object ------------------------------------------------
+
+  emu_reactive <- shiny::reactive({
    emu_theses |>
      dplyr::filter(graduation_year  %in%
                      if('ALL' %in% input$year)
                        unique(emu_theses$graduation_year)
-                     else
+                      else
                        input$year
-                   )
+                   ) |>
+     dplyr::mutate(id = seq_len(dplyr::n()))
 
- })
+   })
 
- # Render plots
+ # Render outputs  ------------------------------------------------------------
 
-  output$distPlot <- shiny::renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = 30)
-
-    # draw the histogram with the specified number of bins
-    hist(
-      x,
-      breaks = bins,
-      col = 'darkgray',
-      border = 'white',
-      xlab = 'Waiting time to next eruption (in mins)',
-      main = 'Histogram of waiting times'
-    )
-  })
-
-
-  output$thesis_location <- leaflet::renderLeaflet({
+  # Map
+   output$thesis_location_map <- leaflet::renderLeaflet({
     data <- emu_reactive()
 
     icons <- leaflet::awesomeIcons(
@@ -84,18 +67,54 @@ app_server <- function(session,input, output) {
       markerColor = "blue"
     )
 
-    emu_reactive() |>
+    data |>
       leaflet::leaflet() |>
       leaflet::addProviderTiles("Stamen.TonerLite") |>
-      leaflet::addAwesomeMarkers(icon = icons,
+      leaflet::addAwesomeMarkers(layerId = data$id,
+                                 icon = icons,
                                  label = data$location,
-                                 popup = paste0("<b>Title:</b> ",
-                                                data$title,
-                                                "<br/> <b>Location:</b> ",
-                                                data$location )
-                                 )
+                                 popup = paste0("<b>Title:</b> ", data$title,
+                                                "<br/>",
+                                                "<b>Location:</b> ", data$location))
+    })
+
+   # Box generated for a single title
+   output$clicked_box <- shiny::renderUI({
+     clicked_id <- input$thesis_location_map_marker_click$id
+
+     clicked_thesis <- emu_reactive() |>
+       dplyr::filter(id  == clicked_id)
+
+     empty_box <- is.null(clicked_id)
+
+     bs4Dash::box(title = clicked_thesis$title,
+                  status = "lightblue",
+                  collapsed = empty_box,
+                  width = NULL,
+                  wordcloud2::wordcloud2Output("wordcloud"))
+
+     })  |>
+     shiny::bindEvent(input$thesis_location_map_marker_click)
 
 
-  })
+
+   # Word cloud of the selected thesis
+   output$wordcloud <- wordcloud2::renderWordcloud2({
+     clicked_id <- input$thesis_location_map_marker_click$id
+
+     clicked_words <- emu_reactive() |>
+       dplyr::filter(id  == clicked_id) |>
+       tidytext::unnest_tokens(word, text_clean) |>
+       dplyr::mutate(word = textstem::lemmatize_words(word))|>
+       dplyr::anti_join(tidytext::stop_words, by = "word") |>
+       dplyr::count(word)|>
+       dplyr::arrange(-n)
+
+     wordcloud2::wordcloud2(clicked_words)
+
+
+     })|>
+    shiny::bindEvent(input$thesis_location_map_marker_click)
+
 
   }
--- R/app_ui.R
+++ R/app_ui.R
@@ -47,8 +47,7 @@ app_ui <- function() {
                                                 bs4Dash::column(12,
                                                                 bs4Dash::box(title = "Links",
                                                                 status = "lightblue",
-                                                                width = NULL,
-                                                                plotOutput("distPlot")
+                                                                width = NULL
                                                                 )),
                                                 bs4Dash::column(3,
                                                        bs4Dash::box(title = "Stats 1",
@@ -69,14 +68,14 @@ app_ui <- function() {
                                                 bs4Dash::column(6,
                                                                 bs4Dash::box(title = "Map plot",
                                                                              width = NULL,
-                                                                             leaflet::leafletOutput("thesis_location")
+                                                                             leaflet::leafletOutput("thesis_location_map")
                                                                              )),
                                                 bs4Dash::column(6,
-                                                                bs4Dash::box(title = "Paper carousel",
-                                                                             width = NULL)),
+                                                                shiny::uiOutput("clicked_box")
+                                                                ),
                                                 bs4Dash::column(12,
                                                                 bs4Dash::box(title = "FAIR",
-                                                                             status = "teal",
+                                                                             status = "lightblue",
                                                                              width = NULL)),
                                                 )
                                               ),
@@ -84,27 +83,32 @@ app_ui <- function() {
                                               shiny::fluidRow(
                                                 bs4Dash::column(6,
                                                                 bs4Dash::box(title = "Top N words",
-                                                                             status = "teal",
+                                                                             status = "lightblue",
                                                                              width = NULL)),
                                                 bs4Dash::column(6,
                                                                 bs4Dash::box(title = "Evolution of top words",
-                                                                             status = "teal",
+                                                                             status = "lightblue",
                                                                              width = NULL)),
                                                 bs4Dash::column(12,
                                                                 bs4Dash::box(title = "Top words per paper",
-                                                                             status = "teal",
-                                                                             width = NULL))
+                                                                             status = "lightblue",
+                                                                             width = NULL,
+                                                                             shiny::radioButtons("tfidf",
+                                                                                                 label = "Choose statistic: ",
+                                                                                                 choices = c("TF", "TF-IDF"),
+                                                                                                 inline = TRUE)
+                                                                             ))
                                                 )
                                               ),
                              bs4Dash::tabItem(tabName = "topic",
                                               shiny::fluidRow(
                                                 bs4Dash::column(12,
                                                                 bs4Dash::box(title = "Models",
-                                                                             status = "teal",
+                                                                             status = "lightblue",
                                                                              width = NULL)),
                                                 bs4Dash::column(12,
                                                                 bs4Dash::box(title = "Model evolution",
-                                                                             status = "teal",
+                                                                             status = "lightblue",
                                                                              width = NULL)),
                                               )
                                               )
