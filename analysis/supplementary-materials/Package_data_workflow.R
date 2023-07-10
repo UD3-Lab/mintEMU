9610fff417b606072e9d6de24239b9cab6edf701
Claudiu Forgaci <33600128+cforgaci@users.noreply.github.com>
2023-07-10 20:32:55 +0200

    Update analysis/supplementary-materials/Package_data_workflow.R
    Co-authored-by: alwil <alexandra.wilczynska@gmail.com>

--- analysis/supplementary-materials/Package_data_workflow.R
+++ analysis/supplementary-materials/Package_data_workflow.R
@@ -1,5 +1,4 @@
 # Create data-raw folder to compute package datasets ----------------------
 usethis::use_data_raw()
 
-# Create data folder with data to be made available as a part of the package
 usethis::use_data(emu, overwrite = TRUE)
