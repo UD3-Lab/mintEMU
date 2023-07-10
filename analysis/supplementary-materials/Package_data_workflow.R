da448c00e6e4f0de577fc3dd8f44062e850386aa
alwil <alexandra.wilczynska@gmail.com>
2023-07-10 15:02:02 +0200

    separated metadata file from the text file

--- analysis/supplementary-materials/Package_data_workflow.R
+++ analysis/supplementary-materials/Package_data_workflow.R
@@ -1,5 +1,5 @@
 # Create data-raw folder to compute package datasets ----------------------
 usethis::use_data_raw()
 
-# Create data folder with data to be made available as a part of the pacakge
+# Create data folder with data to be made available as a part of the package
 usethis::use_data(emu, overwrite = TRUE)
