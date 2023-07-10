4e3817b32ccfc5f4ad05cda4806575128b0afb43
alwil <alexandra.wilczynska@gmail.com>
2023-07-10 13:22:20 +0200

    Update R/clean.R
    Co-authored-by: Claudiu Forgaci <33600128+cforgaci@users.noreply.github.com>

--- R/clean.R
+++ R/clean.R
@@ -183,7 +183,7 @@ short_words <-
 #' coming from two vectors of words,
 #' making sure at least one word from each input vectors is included in the combination
 #'
-#' @param words1 A vector containing single words value to be included in the combination
+#' @param words1 A vector of single words to be included in the combination
 #' @param words2 A vector containing single words to be included in the combination
 #'
 #' @return A vector string consisting of combinations of words coming from input vectors
