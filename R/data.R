fd0863663adc646fa2f0cc6a15143ec1048ff795
Claudiu Forgaci <claudiuforgaci@gmail.com>
2023-12-05 10:34:49 +0100

    Update data documentation

--- R/data.R
+++ R/data.R
@@ -4,13 +4,12 @@
 #' The data frame can be joined with the `emu_metadata` data frame via ID column.
 #'
 #' @format ## `emu_raw`
-#' A data frame with 83 rows and 2 columns:
+#' A data frame with 68 rows and 2 columns.
 #' \describe{
 #'   \item{ID}{ID of the thesis}
 #'   \item{text_raw}{Raw text of the thesis.
-#'   The text has been anonymyzed and Latin special characters have been replaced with 26-letter alphabet counterparts}
-#'
-#' @source <https://repository.tudelft.nl/islandora/search/?collection=education>
+#'   The text has been anonymized, Latin special characters have been replaced with 26-letter alphabet counterparts,
+#'   hyphenation, line breaks and occurrences of the title, often found in the header or footer of theses layouts, have been removed.}
 "emu_raw"
 
 
@@ -20,17 +19,17 @@
 #' The data frame can be joined with the `emu_raw` data frame via ID column.
 #'
 #' @format ## `emu_metadata`
-#' A data frame with 83 rows and 8 columns:
+#' A data frame with 68 rows and 10 columns:
 #' \describe{
 #'   \item{ID}{ID of the thesis}
-#'   \item{graduation_year}{Raw text of the thesis.}
-#'   \item{graduation_semester}{Raw text of the thesis.}
+#'   \item{grad_year}{The year when the thesis was published.}
+#'   \item{grad_sem}{The semester when the student graduated.}
 #'   \item{full_title}{Full title of the thesis.}
 #'   \item{title}{Title of the thesis.}
 #'   \item{subtitle}{Subtitle of the thesis.}
-#'   \item{location}{The location that the thesis is focused on.}
-#'   \item{latitude}{Latitude of the thesis location geocoded from `location`.}
-#'   \item{longitude}{Longitude of the thesis location geocoded from `location`.}
+#'   \item{loc}{The location that the thesis is focused on.}
+#'   \item{lat}{Latitude of the thesis location geocoded from `location`.}
+#'   \item{long}{Longitude of the thesis location geocoded from `location`.}
 #'   \item{abstract}{Abstract of the thesis.}
 #'
 #' @source <https://repository.tudelft.nl/islandora/search/?collection=education>
