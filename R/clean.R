649ceb79329b63e2d710c3a85ddd47a6233e1e3e
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-06-19 11:38:43 +0200

    Update cleaning functions

--- R/clean.R
+++ R/clean.R
@@ -105,7 +105,7 @@ find_meta_stopwords <- function(metadata,
 #' @export
 urbanism_stopwords <- function(add_stopwords = NULL, convert_to_regex = TRUE) {
   stop_words <- paste("\\b",
-                      c(add_stopwords, "city", "cities", "urban", "urbanism", "hab", "km"),
+                      c(add_stopwords |> tolower() |> trimws(), "city", "cities", "urban", "urbanism", "hab", "km"),
                       "\\b", sep = "")
 
   if (convert_to_regex) return(paste(stop_words, collapse = "|"))
@@ -149,7 +149,7 @@ short_words <-
            l_word = 4,
            l_list = 50,
            scope = 'word',
-           print_kable = TRUE) {
+           print_kable = FALSE) {
     if (scope == 'word') {
 
       out_table <-
@@ -244,7 +244,7 @@ word_vec_combos <- function(string1, string2, empty_replacement = "XXXXXXXXXXXXX
 
   # Find combinations with the condition
 
-  combinations <- mapply(word_combos,words1, words2, SIMPLIFY = F )
+  combinations <- mapply(word_combos, words1, words2, SIMPLIFY = F )
 
   combinations <- lapply(combinations, function(x) dplyr::if_else(x == " ", empty_replacement,x))
 
@@ -271,7 +271,7 @@ word_vec_combos <- function(string1, string2, empty_replacement = "XXXXXXXXXXXXX
 #'
 normalise_words <- function(string_vec) {
 
-   words <- strsplit(string_vec, "\\s+")
+  words <- strsplit(string_vec, "\\s+")
 
   # Remove Latin special characters
   normalised_words <- lapply(words, stringi::stri_trans_general, "Latin-ASCII")
@@ -282,24 +282,20 @@ normalise_words <- function(string_vec) {
   # Change all the first letters to upper case
   sentesized_words <- lapply(normalised_words, stringr::str_to_title)
 
-
   # Final list of words
-  final_words <- mapply(c, normalised_words, lowerised_words, sentesized_words, SIMPLIFY=FALSE)
+  final_words <- mapply(c, words, normalised_words, lowerised_words, sentesized_words, SIMPLIFY=FALSE)
 
   # Remove punctuation
-  final_words   <- lapply(final_words,
-                          stringr::str_remove_all, '[[:punct:]]+')
+  final_words <- lapply(final_words,
+                        stringr::str_remove_all, '[[:punct:]]+')
 
+  # add word boundary markers
+  final_words <- lapply(final_words, \(x) dplyr::if_else(is.na(unique(x)), NA_character_,
+                                                         unique(paste0("\\b", x, "\\b"))))
 
-  # embed in the word boundaries
-  final_words   <- lapply(final_words, \(x) dplyr::if_else(is.na(unique(x)), NA_character_,
-                                                           unique(paste0("\\b", x, "\\b"))))
-
-  final_words  <- lapply(final_words ,
-                         \(x) dplyr::if_else(is.na(x), NA_character_,
-                                                    paste(unique(x), collapse = " ")) |> unique()) |> unlist()
-
-
+  final_words <- lapply(final_words,
+                        \(x) dplyr::if_else(is.na(x), NA_character_,
+                                            unique(x))) |> unlist()
 }
 
 
