6ab916905cb312bd0fc393a995d36e7da7b807c4
Claudiu Forgaci <claudiuforgaci@gmail.com>
2024-06-19 11:40:32 +0200

    Make paths in conversion test relative

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -1,18 +1,17 @@
 test_that("convert function works", {
-setwd("C:/Users/skubilay/Desktop/mintEMU")
   # Test input
-  input_text <- "C:/Users/skubilay/Desktop/mintEMU/analysis/data/raw_data/2007-Umemura.pdf"
+  input_text <- here::here("analysis", "data", "raw_data", "2007-Umemura.pdf")
 
   # Expected output
-  expected_output <- "regional metropolis and public transport connection"
+  expected_output <- strsplit("re-generating the shrinking japanese city urban acupuncture junya umemura mentor luisa calabrese", split = " ") |>
+    unlist() |> sort()
 
   # Run the function
-  output <- convert_pdf_text(input_text)
-  matching_excerpt <-stringr::str_detect(output, expected_output)
+  output <- convert_pdf_text(list(input_text), start_pages = "1", end_pages = "1") |> mintEMU::clean_basic() |> strsplit(split = " ") |>
+    unlist() |> sort()
+  # matching_excerpt <- stringr::str_detect(output, expected_output)
 
   # Check the output against the expected result
-  expect_true(matching_excerpt, expected_output,
-              info = "Output should be a lowercase string with no punctuation, extra spaces or numbers."
-  )
+  expect_in(output, expected_output)
 
 })
